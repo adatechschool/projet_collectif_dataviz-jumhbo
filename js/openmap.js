4528c5a17bc11fdfe24a9864f88f7ccca44200a9
Gunduz Mehmet <gunduzmg86@gmail.com>
2022-07-21 10:11:26 +0200

    front

---
+++ js/openmap.js
@@ -0,0 +1,51 @@
+var map = L.map('map').setView([48.874501, 2.3587], 13);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 8,
+    attribution: '© OpenStreetMap'
+}).addTo(map);
+
+var marqueur = L.marker([51.5, -0.09]).addTo(map);
+
+var cercle = L.circle([51.508, -0.11], {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 500
+}).addTo(map);
+
+var polygone = L.polygon([
+    [51.509, -0.08],
+    [51.503, -0.06],
+    [51.51, -0.047]
+]).addTo(map);
+
+marqueur.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+cercle.bindPopup("I am a circle.");
+polygone.bindPopup("I am a polygon.");
+
+var popup = L.popup()
+    .setLatLng([51.513, -0.09])
+    .setContent("Le dernier point!")
+    .openOn(map);
+
+    function onMapClick(e) {
+        alert("You clicked the map at " + e.latlng);
+    }
+    
+    map.on('click', onMapClick);
+
+    var popup = L.popup();
+
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(map);
+}
+
+map.on('click', onMapClick);
+
+
+
+document.querySelector("#modal-dialog > div > div.hoUMge > div > div.yFnP6d > div > div > div > div.eNBuZ > div.MNWp1c > div > iframe")
